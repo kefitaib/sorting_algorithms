df4e21e8c1ddec734f3c496ab6fb51b6ae7bd0ac
kefitaib <taibkefi@yahoo.fr>
2020-06-14 08:51:54 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using the Bubble sort algorithm
+ * @array : the array to sort
+ * @size : size of the array
+ * Return: void
+ */
+
+
+void bubble_sort(int *array, size_t size)
+{
+	int x, b;
+	size_t i, s = size - 1;
+
+	while (s > 0)
+	{
+		for (i = 0; i < s; i++)
+		{
+			b = 0;
+			if (array[i] > array[i + 1])
+			{
+				x = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = x;
+				b = 1;
+				print_array(array, size);
+			}
+		}
+		s--;
+		if (b == 0)
+			s--;
+	}
+}

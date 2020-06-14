3d526840b390ce3bb69be4e2b690700a5b907f97
kefitaib <taibkefi@yahoo.fr>
2020-06-14 09:00:57 -0700

    fix insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *c, *p, *n, *l;
 
-	if (!*list || !(*list)->next)
+	if (!list || !*list || !(*list)->next)
 		return;
 
 	l = (*list)->next;
